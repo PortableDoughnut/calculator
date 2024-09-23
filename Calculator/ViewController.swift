33ac5c5156cc59e2973384d6795113947782a159
PortableDoughnut <gwen.thelin@gmail.com>
2024-09-23 10:29:24 -0600

    An actual fresh start

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -19,12 +19,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultDisplay: UIView!
     @IBOutlet weak var result: UILabel!
     
-    var zeroPressed: Bool = false
-    var isFirstNumber: Bool = true
-    var firstNumber: String = ""
-    var secondNumber: String = ""
-    var operatorPressed: Operators?
-    var operatorExists: Bool = false
     
     func returnDoubleFromString(_ string: String) -> Double {
         guard let double = Double(string) else { return 0 }
@@ -38,92 +32,10 @@ class ViewController: UIViewController {
     }
 
     @IBAction func touchNumber(_ sender: UIButton) {
-        let numberPressed = sender.titleLabel?.text ?? ""
-        let previousResultText = result.text ?? ""
         
-        if previousResultText == "0" {
-            if numberPressed == "0" {
-                zeroPressed = true
-            } else {
-                result.text = numberPressed
-                if isFirstNumber {
-                    firstNumber.append(numberPressed)
-                } else {
-                    secondNumber.append(numberPressed)
-                }
-            }
-        } else {
-            result.text?.append(numberPressed)
-            if isFirstNumber {
-                firstNumber.append(numberPressed)
-            } else {
-                secondNumber.append(numberPressed)
-            }
-            zeroPressed = false
-            
-            print("firstNumber: \(firstNumber)")
-            print("secondNumber: \(secondNumber)")
-            print("operator: \(operatorPressed != nil ? operatorPressed!.rawValue : "none")")
-        }
-    }
-    
-    @IBAction func clearButtonTouched(_ sender: UIButton) {
-        result.text = "0"
-        firstNumber = ""
-        secondNumber = ""
-        isFirstNumber = true
-        operatorExists = false
-        operatorPressed = nil
-    }
-    
-    @IBAction func operatorTouched(_ sender: UIButton) {
-        if !operatorExists {
-            switch sender.titleLabel?.text {
-            case "+":
-                operatorPressed = .addition
-                result.text?.append(" + ")
-            case "-":
-                operatorPressed = .subtraction
-                result.text?.append(" - ")
-            case "x":
-                operatorPressed = .multiplication
-                result.text?.append(" * ")
-            case "÷":
-                operatorPressed = .division
-                result.text?.append(" ÷ ")
-            default:
-                print("No operator found")
-            }
-            
-            isFirstNumber = false
-            operatorExists = true
-        }
     }
     
     @IBAction func equalTouched(_ sender: UIButton) {
-        if !operatorExists {
-            return
-        }
         
-        let answer: String
-        
-        switch operatorPressed {
-        case .addition:
-            answer = String(returnDoubleFromString(firstNumber) + returnDoubleFromString(secondNumber))
-        case .division:
-            answer = String(returnDoubleFromString(firstNumber) + returnDoubleFromString(secondNumber))
-        case .multiplication:
-            answer = String(returnDoubleFromString(firstNumber) + returnDoubleFromString(secondNumber))
-        case .subtraction:
-            answer = String(returnDoubleFromString(firstNumber) + returnDoubleFromString(secondNumber))
-        default:
-            print("Error: No operator pressed")
-            answer = "0"
-        }
-        result.text = answer
-        firstNumber = answer
-        secondNumber = ""
-        operatorExists = false
-        operatorPressed = nil
     }
 }
